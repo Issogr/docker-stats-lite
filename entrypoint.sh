9b0c831fe39980046ca5e315759bb0bb0dc4c9d8
Issogr <grossisimone@outlook.it>
2021-01-28 17:11:07 +0100

    cpu

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,11 @@ round() {
 
 while true; do
   echo $(curl --unix-socket /var/run/docker.sock http://localhost/containers/$CONTAINER_ID/stats?stream=false | jq -c '{"ram":{"rate_usage":((.memory_stats.usage - .memory_stats.stats.cache)/.memory_stats.limit*100),"used_memory": ((.memory_stats.usage - .memory_stats.stats.cache)/1048576), "available_memory": (.memory_stats.limit/1048576)}, "cpu":{"rate_usage":((.cpu_stats.cpu_usage.total_usage - .precpu_stats.cpu_usage.total_usage)/(.cpu_stats.system_cpu_usage - .precpu_stats.system_cpu_usage)*.cpu_stats.online_cpus*100),"cpu_delta": (.cpu_stats.cpu_usage.total_usage - .precpu_stats.cpu_usage.total_usage), "system_cpu_delta":(.cpu_stats.system_cpu_usage - .precpu_stats.system_cpu_usage)}}') >/opt/vol/stats.json
-  sleep 5
   RAM_RATE=$(cat /opt/vol/stats.json | jq '.ram.rate_usage')
-  if [[ $(echo "$RAM_RATE > $RAM_LIMIT" | bc -l) ]]; then
-    echo -e "\nLimite raggiunto: $(round $RAM_RATE 2)\n"
+  CPU_RATE=$(cat /opt/vol/stats.json | jq '.cpu.rate_usage')
+  if [[ $(echo "$RAM_RATE > $RAM_LIMIT" | bc -l) || $(echo "$CPU_RATE > $CPU_LIMIT" | bc -l) ]]; then
+    echo -e "\n📈 RAM: $(round $RAM_RATE 2)"
+    echo -e "📈 CPU: $(round $CPU_RATE 2)\n"
   fi
+  sleep 5
 done
